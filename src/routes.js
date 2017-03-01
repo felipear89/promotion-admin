8718ec619b2ed1fd9e63735bd86dafd268517977
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2017-03-01 10:04:59 -0300

    New promotion route

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,14 @@ import Full from './containers/Full/'
 // import Simple from './containers/Simple/'
 
 import Dashboard from './views/Dashboard/'
+import NewPromotion from './views/NewPromotion'
 
 export default (
   <Router history={hashHistory}>
     <Route path="/" name="Home" component={Full}>
       <IndexRoute component={Dashboard}/>
       <Route path="dashboard" name="Dashboard" component={Dashboard}/>
+      <Route path="promotions/new" name="New Promotion" component={NewPromotion}/>
     </Route>
   </Router>
 );
