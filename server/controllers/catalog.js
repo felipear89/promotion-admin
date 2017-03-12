d538f7849bbd7bf0b62445fb8e3d17f6ba3ed70b
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2017-03-12 14:09:37 -0300

    Add pagination display

--- server/controllers/catalog.js
+++ server/controllers/catalog.js
@@ -16,6 +16,34 @@ exports.search = function(req, res, next) {
     {
       "id": 4,
       "name": "Fake 4",
+    },
+    {
+      "id": 5,
+      "name": "Fake 5",
+    },
+    {
+      "id": 6,
+      "name": "Fake 6",
+    },
+    {
+      "id": 7,
+      "name": "Fake 7",
+    },
+    {
+      "id": 8,
+      "name": "Fake 8",
+    },
+    {
+      "id": 9,
+      "name": "Fake 9",
+    },
+    {
+      "id": 10,
+      "name": "Fake 10",
+    },
+    {
+      "id": 11,
+      "name": "Fake 11",
     }
   ]);
 };
